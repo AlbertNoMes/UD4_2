98604036d648a36b1b21f34efaecbe9a67bfe26d
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-12 00:25:34 +0100

    Variable char

---
+++ src/TresVariablesAPP.java
@@ -0,0 +1,24 @@
+
+public class TresVariablesAPP {
+
+	public static void main(String[] args) {
+		
+		int N=5;
+		double A=4.56;
+		char C='\u0061';
+		
+		System.out.println("Variable N = "+N);
+		System.out.println("Variable A = "+A);
+		System.out.println("Variable C = "+C);
+		
+		double suma=N+A;
+		double resta=A-N;
+		int asciiValue='a';
+		
+		System.out.println("5 + 4.56 = "+suma);
+		System.out.println("4.56 - 5 = "+resta);
+		System.out.println("Valor numérico del carácter a = "+asciiValue);
+
+	}
+
+}
